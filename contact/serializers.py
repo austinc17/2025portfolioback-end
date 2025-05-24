5070eb50d75fb3d08c56f7cef2ae9e23121e05c8
Austin LB Chiwambo <austinlbchiwambo@macbookpro.home>
2025-05-24 20:13:53 +0100

    Initial commit

---
+++ contact/serializers.py
@@ -0,0 +1,10 @@
+from rest_framework import serializers
+from .models import ContactMessage
+
+
+
+
+class ContactSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = ContactMessage
+        fields = ['name', 'email', 'message']
---
+++ contact/views.py
@@ -0,0 +1,28 @@
+
+
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from django.core.mail import send_mail
+from .serializers import ContactSerializer
+from .models import ContactMessage
+
+
+
+class ContactSubmitView(APIView):
+    def post(self, request):
+        serializer = ContactSerializer(data=request.data)
+        if serializer.is_valid():
+            contact = serializer.save()
+
+            # Optional: send an email notification to you
+            send_mail(
+                subject=f"New Contact Form Submission from {contact.name}",
+                message=f"Name: {contact.name}\nEmail: {contact.email}\n\nMessage:\n{contact.message}",
+                from_email=contact.email,
+                recipient_list=['austinchiwambo081@gmail.com'],  
+                fail_silently=False,
+            )
+
+            return Response({'message': 'Success'}, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
