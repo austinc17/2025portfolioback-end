7d766bc84e6c13e5dec55d70d37c5f8b7955a6a4
Austin LB Chiwambo <austinlbchiwambo@macbookpro.home>
2025-05-24 22:12:18 +0100

    Fix CORS: use only live Vercel domain

--- mysite/settings.py
+++ mysite/settings.py
@@ -136,9 +136,7 @@ EMAIL_HOST_PASSWORD = 'tgrk lydt jgcg ztry'
 
 CORS_ALLOWED_ORIGINS = [
     'https://2025portfoliofront-end.vercel.app',
-    'https://2025portfoliofront-g3nzpzpcs-austins-projects-1868d765.vercel.app',
-    'https://2025portfoliofront-end-git-main-austins-projects-1868d765.vercel.app',
-]
+    
 
 CORS_ALLOW_HEADERS = ['*']
 CORS_ALLOW_CREDENTIALS = True
