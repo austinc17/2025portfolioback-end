0a7f1071891f38b192ac6e775c48c0011ae7c668
Austin LB Chiwambo <austinlbchiwambo@macbookpro.home>
2025-05-24 21:14:07 +0100

    Fix CORS origin by removing trailing slash

--- mysite/settings.py
+++ mysite/settings.py
@@ -134,5 +134,5 @@ EMAIL_HOST_USER = 'austinchiwambo081@gmail.com'
 EMAIL_HOST_PASSWORD = 'tgrk lydt jgcg ztry'  
 
 CORS_ALLOWED_ORIGINS = [
-    'https://2025portfoliofront-end.vercel.app/',  
+    'https://2025portfoliofront-end.vercel.app',  
 ]
